d1f75f7a0026d9963cf7a9e1e9477dfbe8a98add
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-11-02 15:08:26 +0000

    create function to add bills amount and expected amount

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -4,43 +4,43 @@ import { connect } from 'react-redux'
 import { useStyles } from '../Layout';
 import { Grid, Paper, } from '@material-ui/core';
 
-export const Dashboard = ({ debt }) => {
+export const Dashboard = ({ debt, bills }) => {
     const classes = useStyles();
-
     const styles = {height: '100px', backgroundColor: '#FFB884'}
     const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
 
     const eachDebt = debt.map(d => Number(d.amount))
-    console.log(debt)
-    console.log(eachDebt)
     const totalDebt = eachDebt.reduce((a, acc) => {
       return a + acc;
     }, 0)
-    
-    // .reduce((add, acc) => {
-    //   return 
-    // })
+
+    const billsAmount = bills
+      .map(b => Number(b.amount))
+      .reduce((d, acc) => d + acc ,0)
+
+    const expectedAmount = billsAmount - totalDebt;
     return (
         <Grid container spacing={3}>
-            <Grid item xs={8} md={3} lg={4}>
+            <Grid item xs={8} md={4} lg={4}>
                 <Paper className={fixedHeightPaper} style={styles}>
                 Total Balance
               </Paper>
             </Grid>
-            <Grid item xs={8} md={3} lg={4}>
+            <Grid item xs={8} md={4} lg={4}>
               <Paper className={fixedHeightPaper} style={styles}>
                 Total Debt:
                 <h4>{totalDebt.toFixed(2)}</h4>
               </Paper>
             </Grid>
-            <Grid item xs={8} md={3} lg={4}>
+            <Grid item xs={8} md={4} lg={4}>
               <Paper className={fixedHeightPaper} style={styles}>
                 Expected Amount
+                <h4>{expectedAmount.toFixed(2)}</h4>
               </Paper>
             </Grid>
-            <Grid item xs={8} md={3} lg={4}>
+            <Grid item xs={8} md={12} lg={4}>
               <Paper className={classes.paper} style={styles}>
-                Orders
+                List
               </Paper>
             </Grid>
           </Grid>
@@ -48,5 +48,7 @@ export const Dashboard = ({ debt }) => {
 }
 const mapStateToProps = state => ({
   debt: state.lumpState.currentUser.splits,
+  bills: state.lumpState.currentUser.bills,
 })
+
 export default connect(mapStateToProps, )(Dashboard);
