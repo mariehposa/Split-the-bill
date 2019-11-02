4be28455d8a5d0a273c423a00362c04817f2bbbb
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-11-02 17:01:00 +0000

    add buttons to dashboard cards

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,58 +1,87 @@
-import React from 'react';
+import React, { useState } from 'react';
 import clsx from 'clsx';
 import { connect } from 'react-redux'
 import { useStyles } from '../Layout';
 import { Grid, Paper, } from '@material-ui/core';
+import { Button } from './dashboardStyle'
 
 export const Dashboard = ({ debt, bills }) => {
-    const classes = useStyles();
-    const styles = {height: '100px', backgroundColor: '#FFB884'}
-    const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
+  const classes = useStyles();
+  const styles = { height: '130px', backgroundColor: '#FFB884' }
+  const styles2 = { backgroundColor: '#FFB884', padding: '20px' }
+  const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
 
-    const eachDebt = debt.map(d => Number(d.amount))
-    const totalDebt = eachDebt.reduce((a, acc) => {
-      return a + acc;
-    }, 0)
+  const eachDebt = debt.map(d => Number(d.amount))
+  console.log(debt);
 
-    const billsAmount = bills
-      .map(b => Number(b.amount))
-      .reduce((d, acc) => d + acc ,0)
+  const allDebts = debt.map(d => {
+    return {
+      amount: d.amount,
+      title: d.bill.title
+    }
+  })
 
-    const expectedAmount = billsAmount - totalDebt;
+  console.log(allDebts);
 
-    const totalBalance = totalDebt + expectedAmount;
 
-    return (
-      <Grid container spacing={3}>
-          <Grid item xs={8} md={4} lg={4}>
-              <Paper className={fixedHeightPaper} style={styles}>
-              Total Balance
+  const totalDebt = eachDebt.reduce((a, acc) => {
+    return a + acc;
+  }, 0)
+
+  const billsAmount = bills
+    .map(b => Number(b.amount))
+    .reduce((d, acc) => d + acc, 0)
+
+  const expectedAmount = billsAmount - totalDebt;
+
+  const totalBalance = totalDebt + expectedAmount;
+
+  const [debtShowing, setDebtShowing] = useState(false)
+
+  return (
+    <Grid container spacing={3}>
+      <Grid item xs={8} md={4} lg={4}>
+        <Paper className={fixedHeightPaper} style={styles}>
+          Total Balance
               <h4>{totalBalance.toFixed(2)}</h4>
-            </Paper>
-          </Grid>
-          <Grid item xs={8} md={4} lg={4}>
-            <Paper className={fixedHeightPaper} style={styles}>
-              Total Debt
+        </Paper>
+      </Grid>
+      <Grid item xs={8} md={4} lg={4}>
+        <Paper className={fixedHeightPaper} style={styles}>
+          Total Debt
               <h4>{totalDebt.toFixed(2)}</h4>
-            </Paper>
-          </Grid>
-          <Grid item xs={8} md={4} lg={4}>
-            <Paper className={fixedHeightPaper} style={styles}>
-              Expected Amount
+        <Button>View</Button>
+        </Paper>
+      </Grid>
+      <Grid item xs={8} md={4} lg={4}>
+        <Paper className={fixedHeightPaper} style={styles}>
+          Expected Amount
               <h4>{expectedAmount.toFixed(2)}</h4>
-            </Paper>
-          </Grid>
-          <Grid item xs={8} md={12} lg={4}>
-            <Paper className={classes.paper} style={styles}>
-              List
-            </Paper>
-          </Grid>
-        </Grid>
-    )
+        <Button>View</Button>
+        </Paper>
+      </Grid>
+      <Grid item xs={8} md={12} lg={4}>
+        <Paper style={styles2}>
+          List
+              {
+            debtShowing ?
+              allDebts.map(d => <div>
+                <h4>{`${d.amount} `} from {` ${d.title}`}</h4>
+              </div>) :
+              allDebts.map(d => <div>
+                <h4>{`${d.amount} `} from {` ${d.title}`}</h4>
+              </div>)
+          }
+        </Paper>
+      </Grid>
+    </Grid>
+  )
 }
+
 const mapStateToProps = state => ({
   debt: state.lumpState.currentUser.splits,
   bills: state.lumpState.currentUser.bills,
+  DebtList: state.lumpState.currentUser.splits.bill,
 })
 
-export default connect(mapStateToProps, )(Dashboard);
+export default connect(mapStateToProps)(Dashboard);
---
+++ src/components/Dashboard/dashboardStyle.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+    color: white;
+    background-color: #B73119;
+    text-transform: uppercase;
+    text-align: center;
+    font-family: system-ui, sans-serif;
+    border-radius: 0.3em;
+    padding: 0.5em;
+    text-decoration: none;
+    border: 0;
+    cursor: pointer;
+    margin: 0 5rem;
+`
